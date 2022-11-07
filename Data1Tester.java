6f1363d3368d581038884295159a11fe74f64654
ItsukiKato <88655924+ItsukiKato@users.noreply.github.com>
2022-11-07 13:16:03 +0900

    課題の締切日をint型で入力させるプログラム

--- Data1Tester.java
+++ Data1Tester.java
@@ -10,21 +10,29 @@ public class Data1Tester {
         Data1[] data = new Data1[number_of_task];
 
         for (int i = 0; i < number_of_task; i++) {
-            data[i] = new Data1("null", "null");
+            data[i] = new Data1("null", 0, 0, 0);
         }
 
         for (int i = 0; i < number_of_task; i++) {
             System.out.print(i + 1 + "番目の課題名:");
             String name = stdIn.next();
 
-            System.out.print(i + 1 + "番目の締切日:");
-            String end_date = stdIn.next();
+            System.out.print(i + 1 + "番目の締切日の年:");
+            int end_year = stdIn.nextInt();
 
-            data[i] = new Data1(name, end_date);
+            System.out.print(i + 1 + "番目の締切日の月:");
+            int end_month = stdIn.nextInt();
+
+            System.out.print(i + 1 + "番目の締切日の日にち:");
+            int end_day = stdIn.nextInt();
+
+            data[i] = new Data1(name, end_day, end_month, end_year);
+
         }
 
         for (int i = 0; i < number_of_task; i++) {
-            System.out.println(i + 1 + "番目の課題:" + data[i].getName() + "締切日:" + data[i].getEndDate());
+            System.out.println(i + 1 + "番目の課題:" + data[i].getName() + "締切日:" + data[i].getEndYear() + "年"
+                    + data[i].getEndMonth() + "月" + data[i].getEndDay() + "日");
         }
 
     }
