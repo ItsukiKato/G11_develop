7687866c39e8318b38f4ccb521d77601447518db
ItsukiKato <88655924+ItsukiKato@users.noreply.github.com>
2022-11-07 15:36:23 +0900

    データクラスを重みを持つコンストラクタに変更

--- Data1.java
+++ Data1.java
@@ -3,13 +3,15 @@ public class Data1 {
     private int end_day;
     private int end_month;
     private int end_year;
+    private int weight;
 
     // コンストラクタ
-    Data1(String name, int end_day, int end_month, int end_year) {
+    Data1(String name, int end_day, int end_month, int end_year, int weight) {
         this.name = name;
         this.end_day = end_day;
         this.end_month = end_month;
-        this.end_year = end_year
+        this.end_year = end_year;
+        this.weight = weight;
     }
 
     // ゲッター
@@ -24,10 +26,15 @@ public class Data1 {
     int getEndMonth() {
         return end_month;
     }
-    
+
     int getEndYear() {
         return end_year;
     }
+
+    int getWeight() {
+        return weight;
+    }
+
     // セッター
     void setName(String name) {
         this.name = name;
@@ -36,12 +43,16 @@ public class Data1 {
     void setEndDay(int end_day) {
         this.end_day = end_day;
     }
-    
+
     void setEndMonth(int end_month) {
         this.end_month = end_month;
     }
-    
+
     void setEndYear(int end_year) {
         this.end_year = end_year;
+    }
+
+    void setWeight(int weight) {
+        this.weight = weight;
     }
 }
