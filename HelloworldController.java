faaf85adae7a81dd5195e053a48ff5944c31d604
tttakumi-gif <109743184+tttakumi-gif@users.noreply.github.com>
2022-11-23 18:50:03 +0900

    Update HelloworldController.java

--- HelloworldController.java
+++ HelloworldController.java
@@ -17,10 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloworldController {
 
-	ArrayList<String> names = new ArrayList<>();
-	ArrayList<Integer> years = new ArrayList<>();
-	ArrayList<Integer> months = new ArrayList<>();
-	ArrayList<Integer> days = new ArrayList<>();
 	ArrayList<Data1> datas = new ArrayList<>();
 	Map<Integer,String> out = new HashMap<>();
 	ArrayList<Data1> sortedlist = new ArrayList<>();
@@ -31,26 +27,23 @@ public class HelloworldController {
   }
 
   @PostMapping("/result")
-  private String confirmMessage(@RequestParam(name = "mess", required = false, defaultValue = "テスト")String mess, 
+  private String confirmMessage(@RequestParam(name = "task", required = false, defaultValue = "2022")String task, 
+		  @RequestParam(name = "mess", required = false, defaultValue = "テスト")String mess, 
 		  @RequestParam(name = "year", required = false, defaultValue = "2022")String year, 
 		  @RequestParam(name = "month", required = false, defaultValue = "12")String month, 
 		  @RequestParam(name = "day", required = false, defaultValue = "24")String day, 
 		  Model model){
-	 //names.add(mess);
 	 int nyear = Integer.parseInt(year);
-	 //years.add(nyear);
 	 int nmonth = Integer.parseInt(month);
-	 //years.add(nmonth);
 	 int nday = Integer.parseInt(day);
-	 //years.add(nday);
 	 
 	 GregorianCalendar gcalendar = new GregorianCalendar();
      
      int current_day2 = gcalendar.get(Calendar.DAY_OF_YEAR);
      
-     int weight = Weighting.weighting(mess, nday, nmonth, nyear, current_day2);
+     int weight = Weighting.weighting(mess, nday, nmonth, nyear, current_day2, task);
      
-     Data1 ndata = new Data1(mess, nday, nmonth, nyear, weight);
+     Data1 ndata = new Data1(task, mess, nday, nmonth, nyear, weight);
      datas.add(ndata);
      
      out.put(weight, mess);
@@ -62,8 +55,9 @@ public class HelloworldController {
      
      QuickSort.quick_sort(tem, 0, (datas.size() - 1));
      
+     sortedlist.clear();
      for(int i = 0; i < tem.length; i++) {
-    	 String temstr = out.get(tem[i]);
+    	 String temstr = out.get(tem[tem.length - i - 1]);
     	 for(Data1 d : datas) {
     		 if(d.getName() == temstr) {
     			 sortedlist.add(d);
@@ -71,13 +65,11 @@ public class HelloworldController {
     	 }
      }
      
-    model.addAttribute("mess1", mess);
     return "/post_message.html";
   }
   
   @PostMapping("/result2")
   private String result(Model model) {
-	  model.addAttribute("demo", sortedlist.get(0).getName());
 	  model.addAttribute("sn", sortedlist);
 	  return "/confirm_message.html";
   }
